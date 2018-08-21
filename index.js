3d5c2d32a309b59b0029e2b2aa583d3722ca5427
randal.sartor@gmail.com <randal.sartor@gmail.com>
2018-08-21 19:47:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function increaseRankBy(n) {
 }
 
 function deepestChild(){
-  var deep = document.getElementById('grand-node').querySelectorAll('div')
+  var deep = document.getElementById('grand-node').querySelectorAll('div').innerHTML
  }
