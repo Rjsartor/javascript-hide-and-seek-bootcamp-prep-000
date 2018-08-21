e2e16223b8ce4ad49bffa3aad83c724cce54dc98
randal.sartor@gmail.com <randal.sartor@gmail.com>
2018-08-21 19:21:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  var rank = document.getElementById('app').querySelectorAll('ranked-list')
+  var rank = document.getElementById('app').querySelectorAll('ranked-list ul')
   
   for (let i = 0; i < rank.length; i++) {
   rank[i].innerHTML = ((parseInt(rank[i].innerHTML)) + n);
