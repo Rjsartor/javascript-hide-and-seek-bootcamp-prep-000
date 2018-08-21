64bcc303e0481d3c07e69cdc04ec44c1b8dda81b
randal.sartor@gmail.com <randal.sartor@gmail.com>
2018-08-21 18:46:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function nestedTarget() {
   return document.getElementById('nested').querySelector('div.target')
 }
 
-// function increaseRankBy(n) {
-//   var ranks = document.getElementByClassName('ranked-list').querySelectorAll('ranked-list.li')
+function increaseRankBy(n) {
+  var ranks = document.getElementByClassName('ranked-list').querySelectorAll('ranked-list.li')
   
-//   for (let i = 0; i < lis.length; i++) {
-//   ranks[i].innerHTML = (i + 1).parseInt();
-//   }
-//   return ranks
-// }
+  for (let i = 0; i < lis.length; i++) {
+  ranks[i].innerHTML = (i + 1).parseInt();
+  }
+  return ranks
+}
