680a2c1ab18fbeaed28831097c7a09a2300b6a4e
randal.sartor@gmail.com <randal.sartor@gmail.com>
2018-08-21 19:34:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  var noder = document.getElementById('grand-node').querySelectorAll('div').innerHTML
+  var noder = document.getElementById('grand-node').querySelectorAll('div')
 }
